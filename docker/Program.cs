57daa8f13017a060b5908f16ed907d91d8e5e13c
Carlos Chacon <caedo.00@gmail.com>
2022-11-08 12:54:44 -0600

    Add docker exercise

---
+++ docker/Program.cs
@@ -0,0 +1,35 @@
+namespace Test
+{
+  static public class Program
+  {
+    public static void Main()
+    {
+      var builder = WebApplication.CreateBuilder();
+
+      // Add services to the container.
+
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
+
+      var app = builder.Build();
+
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+          app.UseSwagger();
+          app.UseSwaggerUI();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseAuthorization();
+
+      app.MapControllers();
+
+      app.Run();
+    }
+
+  }
+}
