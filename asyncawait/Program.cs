4666f83a27f0620c6eeeded9871c45ae387135c4
Carlos Chacon <caedo.00@gmail.com>
2023-09-03 14:24:08 -0600

    Task exercise

---
+++ asyncawait/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+Task task;
+var tokenSource = new CancellationTokenSource();
+
+task = Task
+  .Factory
+  .StartNew(DoStuff, tokenSource.Token)
+  .ContinueWith(_ => task = null);
+
+System.Console.WriteLine("Continuing sync execution");
+Thread.Sleep(2000);
+System.Console.WriteLine("Cancelling task");
+tokenSource.Cancel();
+System.Console.WriteLine("Task cancelled");
+
+var x = Console.ReadLine();
+
+
+void DoStuff()
+{
+  var maxLimit = 10;
+  var i = 0;
+  while(i < maxLimit)
+  {
+    if(!tokenSource.IsCancellationRequested)
+    {
+      System.Console.WriteLine("Sleeping for 1 secs");
+      Thread.Sleep(1000);
+      System.Console.WriteLine("Waking up!");
+      i++;
+    }
+    else
+    {
+      System.Console.WriteLine("Task has been cancelled, exiting...");
+      break;
+    }
+  }
+}
